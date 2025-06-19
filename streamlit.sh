3c910b8b4a16bb7e34a47bf291b93fc19c5753c1
Jiwoo Choi <wldn7568@gmail.com>
2025-06-19 14:58:36 +0900

    sh 수정

--- streamlit.sh
+++ streamlit.sh
@@ -14,15 +14,15 @@ venv\Scripts\activate
 python -m pip install --upgrade pip
 
 # 캐시 없이 패키지 설치
-pip install --no-cache-dir -r requirements.txt
+# pip install --no-cache-dir -r requirements.txt
 
-# pip install streamlit==1.31.0
-# pip install pydantic==2.5.3
-# pip install langchain==0.2.11
-# pip install langchain-openai==0.1.17
-# pip install langchain-core==0.2.23
-# pip install azure-search-documents==11.4.0
-# pip install python-dotenv==1.0.0
+pip install streamlit==1.31.0
+pip install pydantic==2.5.3
+pip install langchain==0.2.11
+pip install langchain-openai==0.1.17
+pip install langchain-core==0.2.23
+pip install azure-search-documents==11.4.0
+pip install python-dotenv==1.0.0
 ## 실행과 동시에 수행, 설치 되어야하는 대상들 전부 다 작성 필요
 
 python -m streamlit azure_connection_test.py
